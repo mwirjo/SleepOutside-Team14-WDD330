922e5eb1c9f100610e85768fd79577df3a3502be
mwirjo <mwirjosentono@byupathway.edu>
2025-11-13 19:01:17 -0300

    completed team activity

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -38,3 +38,31 @@ export function renderListWithTemplate(template, parentElement, list, position =
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.innerHTML = template;
+  // if clear is true we need to clear out the contents of the parent.
+    if (callback) {
+      callback(data);
+    }
+  
+}
+
+export async function loadTemplate(path) {
+    const res = await fetch(path);
+    const template = await res.text();
+    return template;
+}
+
+export async function loadHeaderFooter() {
+    const headerTemplate = await loadTemplate("../partials/header.html");
+
+    const headerElement = document.querySelector("#main-header");
+    const footerTemplate = await loadTemplate("../partials/footer.html");
+
+    const footerElement = document.querySelector("#main-footer");
+  
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
