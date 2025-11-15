d358bd2c6c3dace0dab86a0c598afa6894fff8de
mwirjo <mwirjosentono@byupathway.edu>
2025-11-14 22:09:20 -0300

    commit

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -4,14 +4,13 @@ function productCardTemplate(product) {
   return `
     <li class="product-card">
       <a href="product_pages/?product=${product.Id}">
-        <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
-        <h3 class="card__brand">${product.Brand.Name}</h3>
-        <p class="card__name">${product.NameWithoutBrand}</p>
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h2 class="card__brand">E${product.Brand.Name}</h2>
+        <h3 class="card__name">${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
     </li>`;
 }
-
 
 export default class ProductList {
   constructor(category, dataSource, listElement) {
@@ -20,8 +19,8 @@ export default class ProductList {
     this.listElement = listElement;
   }
 
-   async init() {
-    const list = await this.dataSource.getData(this.category);
+  async init() {
+    const list = await this.dataSource.getData();
     this.renderList(list);
     document.querySelector(".title").textContent = this.category;
   }
