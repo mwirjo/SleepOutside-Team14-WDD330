f79db624bc3e92f06bc24483b092788abbe703f6
somewhat-armoured-shrimp <tor24017@byui.edu>
2025-11-15 13:54:24 -0800

    gonna delete

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
+// const dataSource = new ProductData("tents");
 
-const element = document.querySelector(".product-list");
+// const element = document.querySelector(".product-list");
 
-const productList = new ProductList("tents", dataSource, element); // kill me
+// const productList = new ProductList("tents", dataSource, element); // kill me
 
 
-productList.init();
+// productList.init();
