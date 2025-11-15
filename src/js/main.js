036f9187bc0159e19afbf125b9f59baf8e2a4075
mwirjo <mwirjosentono@byupathway.edu>
2025-11-14 22:04:51 -0300

    completed individual activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-
-const dataSource = new ProductData("tents");
-
-const element = document.querySelector(".product-list");
-
-const productList = new ProductList("tents", dataSource, element); // kill me
-
-
-productList.init();
