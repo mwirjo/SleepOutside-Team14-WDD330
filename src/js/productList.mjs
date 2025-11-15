e16e249869f3b09d1f58f65570ea8608bad412a6
mwirjo <mwirjosentono@byupathway.edu>
2025-11-14 22:15:18 -0300

    commit

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -3,15 +3,15 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
   return `
     <li class="product-card">
-      <a href="product_pages/?product=${product.Id}">
-        <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
-        <h3 class="card__brand">${product.Brand.Name}</h3>
-        <p class="card__name">${product.NameWithoutBrand}</p>
+      <a href="/product_pages/?product=${product.Id}">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}">
+        <h3>${product.Brand.Name}</h3>
+        <p>${product.NameWithoutBrand}</p>
         <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
-    </li>`;
+    </li>
+    `;
 }
-
 
 export default class ProductList {
   constructor(category, dataSource, listElement) {
@@ -20,13 +20,19 @@ export default class ProductList {
     this.listElement = listElement;
   }
 
-   async init() {
+  async init() {
     const list = await this.dataSource.getData(this.category);
     this.renderList(list);
     document.querySelector(".title").textContent = this.category;
   }
 
   renderList(list) {
+    // const htmlStrings = list.map(productCardTemplate);
+    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+
+    // apply use new utility function instead of the commented code above
     renderListWithTemplate(productCardTemplate, this.listElement, list);
+
   }
+
 }
