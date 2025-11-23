b3aae00e427aefa0f988abc2619cee8848c7447b
somewhat-armoured-shrimp <tor24017@byui.edu>
2025-11-22 17:46:03 -0800

    individual activity no stretch

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -14,6 +14,11 @@ document
 // listening for click on the button
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
-
-  order.checkout();
+  const myForm = document.forms[0];
+  const chk_status = myForm.checkValidity();
+  myForm.reportValidity();
+  if(chk_status) {
+    order.checkout();
+  }
+  
 });
